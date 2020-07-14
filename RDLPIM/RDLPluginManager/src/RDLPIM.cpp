f6d786c6c2e67935bfc1636a32b44dcc4ddab6fb
Guy Collins <gwc593@gmail.com>
2020-07-14 09:36:28 +0100

    Static Library architecture for RDPLIM implimented

--- RDLPIM/RDLPluginManager/src/RDLPIM.cpp
+++ RDLPIM/RDLPluginManager/src/RDLPIM.cpp
@@ -1,14 +1,4 @@
-#include "rdlpch.h"
-#include"ConnectionManager/connectionManager.hpp"
-#include"ClientManager/clientManager.hpp"
-#include"RDL/RDL.hpp"
-#include"RDL/rdlData.hpp"
-#include"RequestHandler/dataBase.hpp"
-
-#include"Core/Event.h"
-
-
-
+#include "gwcRDLToolBox.h"
 /*Main RDL Plugin Manager functionality.*/
 int exePluginManager()
 {
